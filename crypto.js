befd6af424ceee3d828af31aae5f4293165f7a5b
anthonphax <anthon_hax>
2024-08-30 16:04:21 -0300

    decrypto

--- crypto.js
+++ crypto.js
@@ -1,5 +1,3 @@
-import {vigenereDecrypt} from './decrypto.js';
-
 export const alfabeto = 'abcdefghijklmnopqrstuvwxyz';
 
 function cesar(msg, chave) {
@@ -54,7 +52,25 @@ function vigenere(msg, chave) {
     console.log(`${cryptograma.join('')}`)
 }
 
+export function vigenereDecrypt(msg, chave) {
+  const diferenca = chave.repeat(Math.ceil(msg.length / chave.length));
+  const plaintext = [];
 
-vigenere("bombeiro xoxana", "fogo");
+  for (let i = 0; i < msg.length; i++) {
+    const msgChar = msg[i];
+    if (msgChar != " ") {
+      const keywordChar = chaveRepeating[i];
+      const shift = alphabet.indexOf(keywordChar);
+  
+      const decryptedChar = alphabet[(alphabet.indexOf(msgChar) - shift + 26) % 26];
+      plaintext.push(decryptedChar);
+    } else {
+      plaintext.push(" ");
+    }
+  }
 
-vigenereDecrypt("gcspjwxcgcspjwxc", "fogo");
+
+  console.log(plaintext.join(''))
+
+  return plaintext.join('');
+}
